3d00077e472079424165a53a98442a0d5d49f26b
David Kooistra <davidkooistra@davids-macbook-pro-2.local>
2017-07-07 16:07:17 -0600

    Finished Practice using menu bars

--- Menu Bars/ViewController.swift
+++ Menu Bars/ViewController.swift
@@ -9,7 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var timer = Timer()
+    var timeInSec = 210
+    
+    
+    
+    
+    @IBOutlet weak var labelTimeDisplay: UILabel!
+    
+    @IBAction func pauseButton(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @IBAction func playButton(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.decreaseTimer), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func minusTenSecButton(_ sender: Any) {
+        if(timeInSec > 10){
+            timeInSec -= 10
+            labelTimeDisplay.text = String(timeInSec)
+        }
+    }
+    @IBAction func addTenSecButton(_ sender: Any) {
+        timeInSec += 10
+        labelTimeDisplay.text = String(timeInSec)
+    }
+    
+    @IBAction func resetButton(_ sender: Any) {
+        timeInSec = 210
+        labelTimeDisplay.text = String(timeInSec)
+    }
+    
+    func decreaseTimer() {
+        if timeInSec > 0 {
+            
+            timeInSec -= 1
+            labelTimeDisplay.text = String(timeInSec)
+            
+        }
+        else {
+            timer.invalidate()
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
